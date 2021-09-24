5d3d1e0562dbb0db30ee2ef03f903c1f7c43265e
unknown <you@example.com>
2021-09-24 14:37:24 -0700

    hola

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Todo} from './../../models/Todo'
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  todos: Todo[];
+  inputTodo:string = "";
+
+  constructor() {
+    
+    
+   }
 
   ngOnInit(): void {
+
+    this.todos = [
+
+    ]
+
   }
+
+  toggleDone(id:number){
+    this.todos.map((v, i) =>{
+      if (i == id)v.completed = !v.completed;
+
+      return v;
+    })
+  }
+
+  deleteTodo(id:number){
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo(){
+    this.todos.push({
+      content:this.inputTodo,
+      completed: false
+    });
+  
+
+  this.inputTodo = "";
+}
 
 }
